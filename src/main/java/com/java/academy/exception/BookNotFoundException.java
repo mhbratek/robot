a04f0918c9a9e4c083e7e32b9fac11b0a3ee6eaa
Pawel Stabryla <Pawel_Stabryla@epam.com>
2017-08-16 16:26:34 +0200

    Add index.md with project about. Update pom.xml dependencies - all maven necessary plugin version; Add plugins to quality control - jacoco, findbugs, checkstyle;

--- src/main/java/com/java/academy/exception/BookNotFoundException.java
+++ src/main/java/com/java/academy/exception/BookNotFoundException.java
@@ -1,8 +1,8 @@
 package com.java.academy.exception;
 
 public class BookNotFoundException extends RuntimeException {
-
-	private static final long serialVersionUID = 2785825571404151841L;
+//
+//	private static final long serialVersionUID = 2785825571404151841L;
 	
 	public BookNotFoundException(String string) {
 		// TODO Auto-generated constructor stub
