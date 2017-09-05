cacd08ef1d8068bd3eb7bc43ac8e1aafc78ac3ab
paulstarbi <Pawel_Stabryla@epam.com>
2017-09-05 19:02:49 +0200

    improve test coverage on Book module

--- Book/src/test/java/com/java/academy/model/CollectionTimeTest.java
+++ Book/src/test/java/com/java/academy/model/CollectionTimeTest.java
@@ -7,6 +7,8 @@ import org.testng.asserts.SoftAssert;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import static org.testng.Assert.assertEquals;
+
 @Test
 public class CollectionTimeTest {
 
@@ -33,6 +35,48 @@ public class CollectionTimeTest {
         sa.assertEquals(collectionTime.getDate(), data);
         sa.assertEquals(collectionTime.getPrice(), book.getPrice());
         sa.assertAll();
+    }
+    @Test (dataProvider = "initData")
+    public void shouldSetCorrectValuesOfCollectionTime(String title, BigDecimal price) {
+        //given
+        Book book = new Book();
+        Date data = new Date();
+        CollectionTime collectionTime = new CollectionTime();
+
+        //when
+        book.setTitle(title);
+        book.setPrice(price);
+        collectionTime.setBook(book);
+        collectionTime.setDate(data);
+        collectionTime.setPrice(book.getPrice());
+
+        //then
+        SoftAssert sa = new SoftAssert();
+        sa.assertEquals(collectionTime.getBook(), book);
+        sa.assertEquals(collectionTime.getDate(), data);
+        sa.assertEquals(collectionTime.getPrice(), book.getPrice());
+        sa.assertAll();
+
+    }
+
+    @Test (dataProvider = "initData")
+    public void shouldReturnCorrectString(String title, BigDecimal price) {
+
+        //given
+        Book book = new Book();
+        Date data = new Date();
+
+        //when
+        book.setTitle(title);
+        book.setPrice(price);
+        CollectionTime collectionTime = new CollectionTime(book, book.getPrice(), data);
+
+        //then
+        assertEquals(collectionTime.toString(), ("CollectionTime{" +
+                "book=" + book +
+                ", price=" + price +
+                ", date=" + data +
+                '}'));
     }
 
 }
